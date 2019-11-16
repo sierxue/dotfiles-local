56f48916fadbec7b244a8351414d85eda4ede306
ddpom <ddpom@outlook>
2019-11-16 15:11:14 +0800

    Add some aliases. Note that python=python3 is required.

--- shell/aliases_local.sh
+++ shell/aliases_local.sh
@@ -1,3 +1,32 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+# a group of git commands
+
+# aliases related git
+alias amp="git add --all && git commit -m 'update' && git push"
+alias gg='git gui'
+
+# https://github.com/ggreer/the_silver_searcher/wiki/Advanced-Usage
+# alias ag="ag --path-to-ignore ~/.ignore" ttst: not working for 0.31.0, shipped with ubuntu 16.04.
+alias ag="ag -p ~/.ignore"
+
+alias grep="grep -I --line-number --color --exclude-dir=\.bzr --exclude-dir=\.git --exclude-dir=\.hg --exclude-dir=\.svn --exclude-dir=build --exclude-dir=dist --exclude=tags"
+
+# http://vim.wikia.com/wiki/Enable_servername_capability_in_vim/xterm
+alias vim='vim --servername vim'
+
+# aliases related to python
+alias jn='jupyter notebook'
+alias python=python3
+# alias pip=pip3
+alias piu='pip3 install --user'
+
 # Open documents
 
 ###############
@@ -34,6 +63,7 @@ alias u_dotfiles='cd ~/.df && ./update_git_repos.sh'
 alias u_vim_anywhere='~/.vim-anywhere/update'
 alias gv_vimrc='gvim ~/.vimrc'
 
+
 ####################
 #  classic papers  #
 ####################
@@ -41,10 +71,7 @@ alias gv_vimrc='gvim ~/.vimrc'
 alias gv_scarf_optimality_1960='gvim ~/repo/sierxue/static/pdf/scarf_optimality_1960.tex'
 
 # za means zathura
-
 alias za_berger='zathura ~/Zotero/storage/AHQ8RKFE/*.pdf'
-
-
 
 
 ###########################
@@ -52,6 +79,7 @@ alias za_berger='zathura ~/Zotero/storage/AHQ8RKFE/*.pdf'
 ###########################
 # gv means gvim.
 
+alias gv_bib='gvim ~/.df/dotfiles-local/texmf/bibtex/bib/ganx.bib'
 alias gv_hist_zsh='gvim ~/.zsh_history'
 alias gv_vimrc='gvim ~/.vimrc'
 alias gv_sty_ganx='gvim ~/.df/dotfiles-local/texmf/tex/latex/ganx.sty'
@@ -59,7 +87,7 @@ alias gv_sty_teaching='gvim ~/.df/dotfiles-local/texmf/tex/latex/beamer_metropol
 alias gv_abolish='gvim ~/.df/dotfiles-local/vim/after/plugin/abolish.vim'
 alias gv_castel='gvim ~/.df/dotfiles-local/vim/UltiSnips/tex.snippets'
 alias gv_ganx='gvim ~/.df/dotfiles-local/vim/UltiSnips/ganxtex.snippets'
-alias v_test_snips='vim ~/.df/dotfiles-local/vim/UltiSnips/test.tex'
+alias gv_test_snips='vim ~/.df/dotfiles-local/vim/UltiSnips/test_ultisnips.tex'
 alias gv_rmd='gvim ~/.df/dotfiles-local/vim/UltiSnips/rmd.snippets'
 alias gv_rmd2='gvim ~/.df/dotfiles-local/vim/UltiSnips/rmd2.snippets'
 alias gv_aliases='gvim ~/.shell/aliases_local.sh'
@@ -112,9 +140,3 @@ alias za_berger='zathura ~/Zotero/storage/AHQ8RKFE/*.pdf'
 #############
 
 alias gv_invcost='gvim ~/repo/invCost/invCost-AP-order-stop.tex'
-
-####################
-#  classic papers  #
-####################
-
-alias gv_scarf_optimality_1960='gvim ~/repo/sierxue/static/pdf/scarf_optimality_1960.tex'
